1b4fdd0281a04703603e2379dbfead74fcc986b2
AJ Keller <pushtheworldllc@gmail.com>
2016-04-27 15:58:57 -0400

    updated new folders

--- include/OpenBCI_32bit_Library.h
+++ include/OpenBCI_32bit_Library.h
@@ -39,8 +39,8 @@ public:
     void leadOffSetForChannel(byte channelNumber, byte pInput, byte nInput);
 
     boolean processChar(char character);
-    void processIncomingLeadOffSettings(void);
-    void processIncomingChannelSettings(void);
+    void processIncomingLeadOffSettings(char character);
+    void processIncomingChannelSettings(char character);
 
     void resetChannelSettingsArrayToDefault(byte channelSettingsArray[][OPENBCI_NUMBER_OF_CHANNEL_SETTINGS]);
     void resetLeadOffArrayToDefault(byte leadOffArray[][OPENBCI_NUMBER_OF_LEAD_OFF_SETTINGS]);
@@ -55,11 +55,25 @@ public:
     void streamStart(void);
     void streamStop(void);
 
+    // OLD CODE REVERT
+    void writeChannelSettings();
+    void writeChannelSettings(byte N);
+    void changeChannelLeadOffDetect();
+    void changeChannelLeadOffDetect(byte N);
+    void configureLeadOffDetection(byte amplitudeCode, byte freqCode);
+
     // Variables
-    boolean streaming;
     boolean daisy;
     boolean sniffMode;
+    boolean streaming;
+    boolean isProcessingIncomingSettingsChannel;
+    boolean isProcessingIncomingSettingsLeadOff;
+
     int boardType;
+    int numberOfIncomingSettingsProcessedChannel;
+    int numberOfIncomingSettingsProcessedLeadOff;
+    char currentChannelSetting;
+    HardwareSerial *_serial;
 
     // Getters
     char getChannelCommandForAsciiChar(char asciiChar);
@@ -78,7 +92,7 @@ public:
     boolean useAux;
     void initialize(void);
     void printAllRegisters(void);
-    byte sendChannelData(void); // send the current data with sample number
+    void sendChannelData(void); // send the current data with sample number
 
 
 // ADS1299
@@ -114,7 +128,7 @@ public:
 
     void changeInputType(byte);
 
-    byte ADS_writeChannelData(void);
+    void ADS_writeChannelData(void);
     // void ADS_printDeviceID(int);   //
     boolean smellDaisy(void);
     void removeDaisy(void);
@@ -139,6 +153,7 @@ public:
     int meanDaisyChannelDataInt[8];
     int numChannels;
     byte channelSettings[OPENBCI_NUMBER_OF_CHANNELS_DAISY][OPENBCI_NUMBER_OF_CHANNEL_SETTINGS];  // array to hold current channel settings
+    byte defaultChannelSettings[OPENBCI_NUMBER_OF_CHANNEL_SETTINGS];  // default channel settings
     byte leadOffSettings[OPENBCI_NUMBER_OF_CHANNELS_DAISY][OPENBCI_NUMBER_OF_LEAD_OFF_SETTINGS];  // used to control on/off of impedance measure for P and N side of each channel
     boolean useInBias[OPENBCI_NUMBER_OF_CHANNELS_DAISY];        // used to remember if we were included in Bias before channel power down
     boolean useSRB2[OPENBCI_NUMBER_OF_CHANNELS_DAISY];
